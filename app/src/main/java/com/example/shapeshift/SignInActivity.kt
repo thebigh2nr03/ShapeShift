7d5626e1a6e9dbac97cb81a4b50af85b4fc45bad
kenny <kenobusiness1@gmail.com>
2023-04-30 14:44:13 -0400

    Create activity_sign_up.xml file, modified build gradle for viewbinding, add colors, themes, kt files

---
+++ app/src/main/java/com/example/shapeshift/SignInActivity.kt
@@ -0,0 +1,4 @@
+package com.example.shapeshift
+
+class SignInActivity {
+}
--- app/src/main/java/com/example/shapeshift/SignUpActivity.kt
+++ app/src/main/java/com/example/shapeshift/SignUpActivity.kt
@@ -1,11 +1,12 @@
 package com.example.shapeshift
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
+
     }
-    }
+}
