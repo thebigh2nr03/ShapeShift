3a5bafbd1a93ac8224a00304953689e2d69cfcbc
thebigh2nr03 <66183008+thebigh2nr03@users.noreply.github.com>
2023-04-30 18:07:20 -0400

    login and signup pages done

--- app/src/main/java/com/usc/shapeshift/SignInActivity.kt
+++ app/src/main/java/com/usc/shapeshift/SignInActivity.kt
@@ -1,4 +1,54 @@
 package com.usc.shapeshift
 
-class SignInActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.usc.shapeshift.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signUpText.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailField.text.toString()
+            val pwd = binding.pwdField.text.toString()
+
+            if (email.isNotEmpty() && pwd.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please enter a password.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/usc/shapeshift/SignUpActivity.kt
+++ app/src/main/java/com/usc/shapeshift/SignUpActivity.kt
@@ -1,12 +1,50 @@
 package com.usc.shapeshift
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.usc.shapeshift.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity(){
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
 
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            val email = binding.emailField.text.toString()
+            val pwd = binding.pwdField.text.toString()
+            val confirmPwd = binding.confirmPwdField.text.toString()
+
+            if (email.isNotEmpty() && pwd.isNotEmpty() && confirmPwd.isNotEmpty()) {
+                if (pwd == confirmPwd) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, SignInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Please enter a password.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
